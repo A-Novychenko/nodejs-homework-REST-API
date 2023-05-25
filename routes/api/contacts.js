04adef8ef2d1c9fbd05f8929904636eb5adb257f
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-05-25 15:09:15 +0300

    add middleware authenticate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,19 +10,27 @@ const {
   updateStatusContact,
 } = require("../../controller/contacts");
 
+const {authenticate} = require("../../middlewares");
+
 const {schemas} = require("../../models/contact");
 const {validateBody, isValidId} = require("../../middlewares");
 
-router.get("/", getAllContacts);
+router.get("/", authenticate, getAllContacts);
 
-router.get("/:contactId", isValidId, getContactById);
+router.get("/:contactId", authenticate, isValidId, getContactById);
 
-router.post("/", validateBody(schemas.addContactSchema), addContact);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addContactSchema),
+  addContact
+);
 
-router.delete("/:contactId", isValidId, removeContact);
+router.delete("/:contactId", authenticate, isValidId, removeContact);
 
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.addContactSchema),
   updateContact
@@ -30,6 +38,7 @@ router.put(
 
 router.patch(
   "/:contactId/favorit",
+  authenticate,
   isValidId,
   validateBody(schemas.updateStatusContactSchema),
   updateStatusContact
