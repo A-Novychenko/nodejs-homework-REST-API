39557fa02d8365f188c62f4c06e9bc3cc3fa82ef
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-05-31 15:50:16 +0300

    login jest-testing done

--- controller/auth/login.test.js
+++ controller/auth/login.test.js
@@ -1,10 +1,11 @@
 // ответ должен иметь статус-код 200 ---ГОТОВО
-// в ответе должен возвращаться токен
+// в ответе должен возвращаться токен ---  ГОТОВО
 // в ответе должен возвращаться объект user с 2 полями email и subscription, имеющие тип данных String
 
 const express = require("express");
 const request = require("supertest");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const {DB_HOST, PORT = 3000} = process.env;
@@ -19,6 +20,11 @@ app.use(express.json());
 app.post("/api/login", login);
 
 describe("test register controller", function () {
+  const body = {
+    email: "TEST@example.com",
+    password: "examplepassword",
+  };
+
   beforeAll(async () => {
     await mongoose.connect(DB_HOST);
   });
@@ -30,12 +36,23 @@ describe("test register controller", function () {
   });
 
   it("response has status code 200", async () => {
-    await request(app)
-      .post("/api/login")
-      .send({
-        email: "TEST@example.com",
-        password: "examplepassword",
-      })
-      .expect(200);
+    const response = await request(app).post("/api/login").send(body);
+    expect(response.status).toBe(200);
+  });
+
+  it("response has token", async () => {
+    const response = await request(app).post("/api/login").send(body);
+
+    expect(
+      jwt.decode(response.body.data.token, {complete: true}).header.typ
+    ).toBe("JWT");
+  });
+
+  it("a user object is returned with 2 fields email and subscription, both of which have a String data type", async () => {
+    const response = await request(app).post("/api/login").send(body);
+
+    expect(typeof response.body.data.user).toBe("object");
+    expect(typeof response.body.data.user.email).toBe("string");
+    expect(typeof response.body.data.user.subscription).toBe("string");
   });
 });
